0d6fa61edf98f42358736475fc947c013aea7957
partoo <partoo@163.com>
2021-09-25 21:17:51 +0800

    fixed config file key name

--- src/Core/Code/SimpleCode.php
+++ src/Core/Code/SimpleCode.php
@@ -10,7 +10,7 @@ class SimpleCode extends AbstractCode
 
     protected function asciiCode(): string
     {
-        $length = config('tao.captcha.length');
+        $length = config('captcha.length');
         $code = Helper::shuffleString($this->num_config['chars'], $length[0], $length[1]);
         $this->realCode = strtoupper($code);
         return $code;
@@ -18,7 +18,7 @@ class SimpleCode extends AbstractCode
 
     protected function nonAsciiCode(): string
     {
-        $length = config('tao.captcha.length');
+        $length = config('captcha.length');
         $code = Helper::shuffleString($this->num_config['chars'], $length[0], $length[1]);
         $this->realCode = Helper::chineseNumToArabicNum($code);
         return $code;
